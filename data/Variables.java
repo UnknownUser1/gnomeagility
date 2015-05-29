84094a265697ec3061cdddba8e6521237272dd3e
UnknownUser <ethanspeeder1@hotmail.com>
2015-05-29 20:14:57 +1000

    Revert "Update"
    This reverts commit 24ae31a284dcdb3d351e10877e3b9d6bdc96ef9c.

--- data/Variables.java
+++
@@ -1,9 +0,0 @@
-package gnomeagility.data;
-
-import org.rev317.min.api.methods.SceneObjects;
-
-public class Variables {
-	public static boolean isAtCourse() {
-		return SceneObjects.getNearest(2285, 2313, 2314).length > 0;
-	}
-}
